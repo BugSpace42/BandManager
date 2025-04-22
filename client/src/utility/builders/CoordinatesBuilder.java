fd14e8ead06feeb87b164f42cbb83c21d856961b
BugSpace <409478@niuitmo.ru>
2025-04-22 06:20:25 +0300

    add type validator

--- client/src/utility/builders/CoordinatesBuilder.java
+++ client/src/utility/builders/CoordinatesBuilder.java
@@ -11,7 +11,7 @@ public class CoordinatesBuilder {
      * Собирает объект класса Coordinates
      * @param x - первая координата
      * @param y - вторая координата
-     * @return бъект класса Coordinates
+     * @return объект класса Coordinates
      */
     public static Coordinates build(Integer x, long y) {
         Coordinates coordinates = new Coordinates(x, y);
